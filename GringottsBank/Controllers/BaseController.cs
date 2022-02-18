624250114a68b5777efc39efa4e10fd6e639d5a3
Rahul Kumar Parihar <rahulkumarpariharmailbox@gmail.com>
2022-02-18 10:38:25 +0530

    missing change

---
+++ GringottsBank/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using GringottsBank.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GringottsBank.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected IActionResult EndpointResponse<T>(ResponseModel<T> dto)
+        {
+            if (dto.HasError)
+            {
+                return BadRequest((Error<T>)dto);
+            }
+            else
+            {
+                return Ok((Success<T>)dto);
+            }
+        }
+    }
+}
