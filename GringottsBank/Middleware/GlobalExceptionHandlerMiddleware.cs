0926a6417b05ee90339288c5723727d17158c2cf
Rahul Kumar Parihar <rahulkumarpariharmailbox@gmail.com>
2022-02-19 11:21:35 +0530

    Adding Global Exception Handler

---
+++ GringottsBank/Middleware/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using GringottsBank.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace GringottsBank.Middleware
+{
+    public class GlobalExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public GlobalExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext httpContext, Exception exception)
+        {
+            httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            httpContext.Response.ContentType = "application/json";
+            httpContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            Error<string> error = new Error<string>();
+            error.ErrorMessages.Add(exception.Message);
+
+            await httpContext.Response.WriteAsync(JsonSerializer.Serialize<Error<string>>(error));
+        }
+    }
+}
